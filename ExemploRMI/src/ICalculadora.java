b45bc0e362428e07181dcaefc9b986f860792c81
JoyceQuintino <joycequintino11@gmail.com>
2021-07-04 16:55:05 -0300

    Adição dos métodos subtração, divisão e multiplicação na interface

--- ExemploRMI/src/ICalculadora.java
+++ ExemploRMI/src/ICalculadora.java
@@ -1,7 +1,17 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/*Toda interface que extende de Remote deve usar o RemoteException.
+Existem um conjunto de problemas que podem acontecer, por exemplo, 
+a rede cair, o objeto remoto cair e o RemoteException alerta sobre esses problemas.
+*/
 public interface ICalculadora extends Remote{
 
-	public int soma(int a, int b) throws RemoteException;
+	public double soma(double a, double b) throws RemoteException;
+	
+	public double subtracao(double a, double b) throws RemoteException;
+	
+	public double divisao(double a, double b) throws RemoteException;
+	
+	public double multiplicacao(double a, double b) throws RemoteException;
 }

273642bbf38b29afe9696a57d429a0e812e067f8
JoyceQuintino <joycequintino11@gmail.com>
2021-07-04 01:18:14 -0300

    Modificação na solicitação dos dados a partir do teclado

--- JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
+++ JavaClientSocket_Calculadora/src/CalculadoraClientSocket.java
@@ -9,50 +9,64 @@ public class CalculadoraClientSocket {
 
 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub
-		Scanner read = new Scanner(System.in);
-		double oper1,oper2;
-		int operacao; 
-		String result="";
+		int num=1;
 		
-		System.out.printf("Informe o primeiro valor: \n");
-		oper1 = read.nextDouble();
-		System.out.printf("Informe o segundo valor: \n");
-		oper2 = read.nextDouble();
-		//1-somar 2-subtrair 3-dividir 4-multiplicar
-		System.out.printf("Informe a operação:\n "
-				+ "1 - Soma\n 2 - Subtração\n 3 - Divisão\n 4 - Multiplicação\n");
-		operacao = read.nextInt();
-		
-        try {
-        	//Conexão com o Servidor
-            Socket clientSocket = new Socket("192.168.0.123", 9090);
-            //Cria um fluxo de saída dos dados
-            //O getOutputStream() envia os dados para o servidor em bytes
-            DataOutputStream socketSaidaServer = new DataOutputStream(clientSocket.getOutputStream());
-            
-            //Enviando os dados
-            socketSaidaServer.writeBytes(operacao+"\n");
-            socketSaidaServer.writeBytes(oper1+ "\n");
-            socketSaidaServer.writeBytes( oper2+ "\n");
-            socketSaidaServer.flush(); //foça que os dados sejam enviados
+		do {
+			// TODO Auto-generated method stub
+			Scanner read = new Scanner(System.in);
+			double oper1,oper2;
+			int operacao; 
+			String result="";
+			
+			System.out.printf("Informe o primeiro valor: \n");
+			oper1 = read.nextDouble();
+			System.out.printf("Informe o segundo valor: \n");
+			oper2 = read.nextDouble();
+			if(oper2 == 0) {
+				System.out.printf("Divisão impossível de ser realizada.\n");
+			}
+			//1-somar 2-subtrair 3-dividir 4-multiplicar
+			System.out.printf("Informe a operação:\n "
+					+ "1 - Soma\n 2 - Subtração\n 3 - Divisão\n 4 - Multiplicação\n");
+			operacao = read.nextInt();
+			
+	        try {
+	        	//Conexão com o Servidor
+	            Socket clientSocket = new Socket("192.168.0.123", 9090);
+	            //Cria um fluxo de saída dos dados
+	            //O getOutputStream() envia os dados para o servidor em bytes
+	            DataOutputStream socketSaidaServer = new DataOutputStream
+	            		(clientSocket.getOutputStream());
+	            
+	            //Enviando os dados
+	            socketSaidaServer.writeBytes(operacao+"\n");
+	            socketSaidaServer.writeBytes(oper1+ "\n");
+	            socketSaidaServer.writeBytes( oper2+ "\n");
+	            socketSaidaServer.flush(); //foça que os dados sejam enviados
 
-            //Recebendo a resposta
-            //É através do método getInputStream() que o servidor consegue capturar o que o cliente está enviado.
-            //O objeto messageFromServer recebe os dados enviados para o servidor
-            BufferedReader messageFromServer = new BufferedReader
-                    (new InputStreamReader(clientSocket.getInputStream()));
-            //Ler uma linha por vez dos dados enviados para o servidor e armazena na variável result.
-            result=messageFromServer.readLine();
-            
-            //Exibe os dados
-            System.out.println("resultado= "+result);
-            //Fecha a conexão com o servidor
-            clientSocket.close();
+	            //Recebendo a resposta
+	            //É através do método getInputStream() que o servidor consegue capturar o que o cliente está enviado.
+	            //O objeto messageFromServer recebe os dados enviados para o servidor
+	            BufferedReader messageFromServer = new BufferedReader
+	                    (new InputStreamReader(clientSocket.getInputStream()));
+	            //Ler uma linha por vez dos dados enviados para o servidor e armazena na variável result.
+	            result=messageFromServer.readLine();
+	            
+	            //Exibe os dados
+	            System.out.println("resultado= "+result);
+	            //Fecha a conexão com o servidor
+	            clientSocket.close();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	        System.out.printf("Deseja continuar?\n 0 - Encerrar\n 1 - Continuar\n");
+			num = read.nextInt();
+			if(num==0) {
+				System.out.printf("Fim!\n");
+			}
+		}while(num>0);
+
 	}
 
 }
